d10609d149765ff385eed1fc5ad352ff3a6685bc
prithvish774848474 <prithvish.freelance@gmail.com>
2025-08-11 17:22:46 +0530

    Adding initial files

---
+++ Backend/connection.ts
@@ -0,0 +1,25 @@
+// MySQL connection.
+import { drizzle } from "drizzle-orm/mysql2";
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./.env", quiet: true });
+
+if (!process.env.DB_PASSWORD) {
+  console.error("Cannot find environment variable.");
+  process.exit(1);
+}
+
+// Creating a Connection Pool from Connection String.
+const pool: mysql.Pool = mysql.createPool({
+  host: process.env.DB_HOSTNAME,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10, // number of connections in pool.
+  queueLimit: 0,
+});
+pool && console.log("Connected to MySQL Database!");
+export const db = drizzle(pool);
